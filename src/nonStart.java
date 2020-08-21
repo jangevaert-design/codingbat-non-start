950b09c20865e9cff6682fd4574ccb141bf53ec5
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-21 06:11:41 -0600

    Initial commit

---
+++ src/nonStart.java
@@ -0,0 +1,9 @@
+public class nonStart {
+  public String nonStart(String a, String b) {
+    String str1 = a.substring(1, a.length());
+    String str2 = b.substring(1, b.length());
+    return str1 + str2;
+
+  }
+
+}
